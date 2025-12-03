557769d4b4b6567bbdf427d9688360d8615523e1
hiroki mizuno <act.aida@outlook.jp>
2025-12-03 16:49:18 +0900

    全員のDBアクセスを確認したので、今後のDBアクセスのやり方をaccessTestDB.pyに記載します

--- accessTestDB.py
+++ accessTestDB.py
@@ -1,8 +1,10 @@
+import os
+# 必要に応じて追加のインポート　pip install python-dotenv
+from dotenv import load_dotenv
 import psycopg2
 
-conn = psycopg2.connect(
-    "postgresql://postgres.tjuwdwusejfkzceqxgwp:Idea001!@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres?sslmode=require"
-)
+load_dotenv()
+conn = psycopg2.connect(os.getenv("SUPABASE_DB_URL"))
 
 cur = conn.cursor()
 cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public';")
